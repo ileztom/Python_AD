9e73dd790f508c4f9eaec932d88ffcaa7bf1f0a0
ileztom <91593668+ileztom@users.noreply.github.com>
2025-06-03 16:52:53 +0300

    Delete test_calculator/src/main/java/practice/pagepattern directory

--- test_calculator/src/main/java/practice/pagepattern/AbstractPage.java
+++
@@ -1,8 +0,0 @@
-package practice.pagepattern;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-public abstract class AbstractPage {
-    public abstract WebDriver getDriver();
-}
